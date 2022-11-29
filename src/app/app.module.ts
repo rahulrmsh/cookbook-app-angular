e2e01759c4b6334ed0261234b72edcfc3f25be49
rahulrmsh <rahulramesh13.rr@gmail.com>
2022-11-29 11:35:49 +0530

    [Rahul] | Add. Initial Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipies/recipeList/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shoppingList/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, RecipiesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent, ShoppingListComponent, ShoppingListEditComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
